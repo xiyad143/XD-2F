707f16554de6e83023dc2cdc004bc12d6ba08a98
XIYAD <118003449+xiyad143@users.noreply.github.com>
2023-09-17 23:35:58 +0600

    Update xd-twf.py

--- xd-twf.py
+++ xd-twf.py
@@ -20,7 +20,7 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('twf.so'):
-        os.system('curl -L https://github.com/xiyad143/XD-2F/blob/main/twf.cpython-311.so?raw=true -o xd.so') 
+        os.system('curl -L https://github.com/xiyad143/XD-2F/blob/main/twf.cpython-311.so?raw=true -o twf.so') 
         import twf  
     else:
         import twf
